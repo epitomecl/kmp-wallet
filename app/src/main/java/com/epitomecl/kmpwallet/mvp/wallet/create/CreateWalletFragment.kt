a0c2ec1889ec53fdff85ff926bbd39181ccd3d7b
jsyun-epi <jsyun@epitomecl.com>
2018-06-01 00:45:51 +0900

    add spinner

--- app/src/main/java/com/epitomecl/kmpwallet/mvp/wallet/create/CreateWalletFragment.kt
+++ app/src/main/java/com/epitomecl/kmpwallet/mvp/wallet/create/CreateWalletFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import com.epitomecl.kmpwallet.R
 import com.epitomecl.kmpwallet.mvp.base.BaseFragment
 import android.view.animation.TranslateAnimation
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_createwallet.*
 
@@ -14,7 +16,7 @@ class CreateWalletFragment : BaseFragment<CreateWalletContract.View,
         CreateWalletContract.Presenter>(),
         CreateWalletContract.View {
 
-    private val create = "Press button it will make '%s' wallet"
+    private val create = "3. Press button it will make '%s' wallet"
 
     override var mPresenter: CreateWalletContract.Presenter = CreateWalletPresenter()
 
@@ -41,6 +43,8 @@ class CreateWalletFragment : BaseFragment<CreateWalletContract.View,
         tvCreateMsg.text = create.format("BTC")
 
         btnCreateWallet.setOnClickListener { onClickCreate() }
+
+        initCryptoTypeSpinner()
     }
 
     private fun onClickCreate() {
@@ -57,6 +61,21 @@ class CreateWalletFragment : BaseFragment<CreateWalletContract.View,
         Toast.makeText(context, "wallet label is too short.", Toast.LENGTH_SHORT).show()
 
         return false
+    }
+
+    private fun initCryptoTypeSpinner() {
+        val cryptoTypes = arrayOf(getString(R.string.crypto_btc_big), getString(R.string.crypto_bch_big), getString(R.string.crypto_eth_big))
+        spCryptoType.adapter = ArrayAdapter(context, android.R.layout.simple_spinner_item, cryptoTypes)
+        spCryptoType.setPromptId(R.string.msg_select_crypto_type)
+        spCryptoType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Toast.makeText(context, cryptoTypes[position], Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
 }
